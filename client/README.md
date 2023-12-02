5a184b21c9e86a03333f175f28825864900c49b1
Matt Wynne <matt@mattwynne.net>
2023-12-02 09:37:36 -0800

    Add client chat app

---
+++ client/README.md
@@ -0,0 +1,11 @@
+# Chat app
+
+To play:
+
+    npm run dev
+
+## Credits
+
+- Icons made by <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+
+- Based on https://github.com/vnglst/svelte-gundb-chat by Koen van Gilst
---
+++ client/src/App.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { nav } from "./stores";
+  import Messages from "./Messages.svelte";
+  import Settings from "./Settings.svelte";
+  import Nav from "./ui/Nav.svelte";
+  import Page from "./ui/Page.svelte";
+  import Footer from "./Footer.svelte";
+</script>
+
+{#if $nav === 'settings'}
+  <Page>
+    <Nav>Sign In</Nav>
+    <Settings />
+    <Footer />
+  </Page>
+{:else if $nav === 'messages'}
+  <Page>
+    <Nav
+      showBack
+      backText="Sign In"
+      on:back={() => {
+        $nav = 'settings';
+      }}
+    >
+      Messages
+    </Nav>
+    <Messages />
+  </Page>
+{/if}
