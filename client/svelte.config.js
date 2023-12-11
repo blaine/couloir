5e60d6fb54735ed9fa0b944e4612e3faa13a0477
Matt Wynne <matt@mattwynne.net>
2023-12-11 14:35:56 -0700

    Bring back vite preprocessor so we can still build typescript
    Ref #5

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,9 +1,9 @@
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import adapter from "@sveltejs/adapter-static";
 
 export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
+  preprocess: vitePreprocess(),
   kit: {
     adapter: adapter(),
   },
-};
+}
