9d246cb46a3d011c02d3b06253416c122ba1b843
Matt Wynne <matt@mattwynne.net>
2023-12-12 22:57:27 -0800

    Remove old TODO

--- fake-server/src/app.ts
+++ fake-server/src/app.ts
@@ -113,7 +113,6 @@ export default (dataPath: string) => {
   app.post("/messages", async (req, res) => {
     // we do the stringification server-side to ensure that the client can't send
     // a malformed message
-    // TODO: do we need to worry about this? we could have the client send both the ID and some text, and we just act as a key/value store -- @matt
     let message = JSON.stringify(req.body)
     let hash = crypto.createHash("sha256").update(message).digest("hex")
     await fs.writeFile(`${dataPath}/${hash}`, JSON.stringify(req.body))
